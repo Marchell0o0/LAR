Polynomial interpolation for lookahead distance, and miscellaneus fixes

environment.py
@@ -6,18 +6,18 @@ def distance(point1, point2) -> float:
 class Robot:
     def __init__(self, x, y, a,):
 
-        self.max_detection_range = 1.1 # m
+        self.max_detection_range = 1.5 # m
         self.fov_angle = np.deg2rad(70)
         self.radius = 0.177 # m
-        self.linear_speed = 0.5 # m/s
-        self.linear_acceleration = 1 # m/s^2
-        self.max_angular_speed = np.pi / 4 # rad/s
-        self.minimal_angular_speed = 0.5
+        self.linear_speed = 0.4 # m/s
+        self.linear_acceleration = 0.4 # m/s^2
+        self.max_angular_speed = np.pi / 6 # rad/s
+        self.minimal_angular_speed = 0.0
         self.minimal_linear_speed = 0.0
         
         self.distance_allowence = 0.03
         self.angle_allowence = np.deg2rad(2)
-        self.obstacle_clearence = 0 # m
+        self.obstacle_clearence = 0.045 # m
         self.lookahead_point_distancing = np.deg2rad(5)
         
         self.x = x
@@ -132,6 +132,7 @@ class Environment:
             self.checkpoints[-1].y = new_y
             self.checkpoints[-1].a = checkpoint_angle
             updated = True
+            self.found_finish = True
             return
 
         if not updated:
@@ -139,6 +140,7 @@ class Environment:
             new_checkpoint = Checkpoint(new_x, new_y, checkpoint_angle)
             self.checkpoints.append(new_checkpoint)
             self.found_finish = True
+            print("--------------- Found FINISH ---------------")
         return
 
     def update_checkpoints(self):
@@ -162,8 +164,8 @@ class Environment:
                         
     def simulate_movement(self, move, time):
         # Introduce noise in linear and angular velocities
-        linear_noise =  np.random.normal(0, 0.05)  # Mean 0, standard deviation 0.05
-        angular_noise = np.random.normal(0, 0.01) # Mean 0, standard deviation 0.01
+        linear_noise = 0 * np.random.normal(0, 0.05)  # Mean 0, standard deviation 0.05
+        angular_noise = 0 * np.random.normal(0, 0.1) # Mean 0, standard deviation 0.01
 
         # Apply the noise to the movement values
         noisy_linear = move[0] + linear_noise
@@ -198,8 +200,8 @@ class Environment:
                     -self.real_robot.fov_angle / 2 <= true_angle_to_obstacle <= self.real_robot.fov_angle / 2):
                 
                 # Add noise to distance and angle measurements
-                distance_noise =  np.random.normal(0, 0.03)
-                angle_noise =  np.random.normal(0, np.radians(1))
+                distance_noise = 0 * np.random.normal(0, 0.03)
+                angle_noise = 0 * np.random.normal(0, np.radians(1))
 
                 # Ensure that the measurements are scalar
                 measured_distance = min(self.real_robot.max_detection_range, float(true_distance_to_obstacle + distance_noise))

kalman.py
@@ -9,7 +9,7 @@ class KalmanFilter:
         self.Q_obstacles = np.diag([0.05, np.deg2rad(1), 0.001]) # sigma r, phi, color
         
         # acceptable mahalanobis distance
-        self.alpha = 0.2
+        self.alpha = 0.4
         
         self.mu = np.array([[self.env.robot.x],[self.env.robot.y],[self.env.robot.a]], dtype=np.float64)
 

main.py
@@ -77,9 +77,9 @@ def main():
     #                      Checkpoint(2, 1, np.pi/4),
     #                      Checkpoint(1, 0, -np.pi)],
     #                      [],
-    #                     [Obstacle(0.50, -0.20, 0),
-    #                      Obstacle(0.50, 0.20, 1),
-    #                      Obstacle(0.50, -0.50, 2),
+    #                     [Obstacle(0.50, -0.15, 0),
+    #                      Obstacle(0.50, 0.15, 1),
+    #                      Obstacle(0.50, -0.30, 2),
     #                      Obstacle(1.5, 0.50, 1),
     #                      Obstacle(1.5, 0.75, 2),
     #                      ])
@@ -101,22 +101,25 @@ def main():
     #                     [],
     #                     [Obstacle(-0.7, -0.7, 0)
     #                      ])
-    # env = Environment(Robot(0, 0, 0), 
+    # env = Environment(Robot(0, 0, 0), Robot(0, 0, 0),
+    #                     [Checkpoint(1, 1, 0),
+    #                      Checkpoint(0, 0, 0)],
     #                     [],
-    #                     [],
-    #                     [Obstacle(0.4, 0.1, 0)
-    #                      ])
+    #                     [Obstacle(0.5, 0.5, 0)])
     
     
     env = Environment(Robot(0, 0, 0), Robot(0, 0, 0),
-                        [Checkpoint(0, 0, np.pi/2),
-                         Checkpoint(0, 0, np.pi),
-                         Checkpoint(0, 0, -np.pi/2),
-                         Checkpoint(0, 0, 0)],
+                        [
+                        # Checkpoint(0, 0, np.pi/2),
+                        #  Checkpoint(0, 0, np.pi),
+                        #  Checkpoint(0, 0, -np.pi/2),
+                        #  Checkpoint(0, 0, 0)
+                        ],
                          [],
                          [Obstacle(1, 0.05, 0),
                          Obstacle(1, -0.05, 1),
-                         Obstacle(1.,1, 1),
+                         Obstacle(0.6, 0.6, 2),
+                         Obstacle(1, 1, 1),
                          Obstacle(0.95, 1.05, 0),
                          Obstacle(0, 1.50, 2),
                          Obstacle(0.05, 1.55, 2),
@@ -135,13 +138,14 @@ def main():
     if turtlebot:
         
         turtle, rate = initialize_turtle()
-        color_settings = ColorSettings()
-        color_settings.calibrate_color(turtle)
+        # color_settings = ColorSettings()
+        # color_settings.calibrate_color(turtle)
     
+        turtle.reset_odometry()
+        previous_odometry = turtle.get_odometry()
     
     running = True
     counter = 0
-    previous_odometry = (env.robot.x, env.robot.y, env.robot.a)
     previous_time =  0
     obstacle_measurements = []
     next_move = (0, 0)
@@ -179,20 +183,21 @@ def main():
             next_move = path_execution.get_current_move()
             turtle.cmd_velocity(angular = next_move[1], linear = next_move[0])
             # move change, record time from here
-            previous_time = time.time()
+            # previous_time = time.time()
             rate.sleep()
 
             if next_move[0] == 0 and next_move[1] == 0:
-                print("Robot has stopped, making a measurement")
-                time.sleep(0.5)
-                image = turtle.get_rgb_image()
-                pc_image = turtle.get_point_cloud()
+                # print("Robot has stopped, making a measurement")
+                # time.sleep(0.5)
+                # image = turtle.get_rgb_image()
+                # pc_image = turtle.get_point_cloud()
 
-                rectg_processor = RectangleProcessor(image,
-                                                pc_image,
-                                                color_settings)
-                detected_rectgs, masked_rectgs, image_rectg  = rectg_processor.process_image()
-                obstacle_measurements = detected_rectgs
+                # rectg_processor = RectangleProcessor(image,
+                #                                 pc_image,
+                #                                 color_settings)
+                # detected_rectgs, masked_rectgs, image_rectg  = rectg_processor.process_image()
+                # obstacle_measurements = detected_rectgs
+                obstacle_measurements = env.get_measurement()
             else:
                 obstacle_measurements = []
 
@@ -213,16 +218,24 @@ def main():
             # update mu_t-1 to mu_t
             kalman_filter.pos_update(odometry_change)
             
+            env.real_robot.x = env.robot.x
+            env.real_robot.y = env.robot.y
+            env.real_robot.a = env.robot.a
+
             # measurements are from mu_t
             kalman_filter.obstacles_measurement_update(obstacle_measurements)
             kalman_filter.update_for_visualization()
             
+            env.update_checkpoints()
+
             path_execution.update_path()
             
         else:
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 running = False
             if counter > 20:
+                # print(env.robot.a)
+                # print("Current path:", path_execution.path)
                 next_move = path_execution.get_current_move()
                 
                 # move change, record time from here

path_execution.py
@@ -14,6 +14,7 @@ class PathExecution:
         self.current_checkpoint_idx = 0
         
         self.max_lookahead_distance = 0.3
+        self.min_lookahead_distance = 0.05
         self.current_lookahead_distance = self.max_lookahead_distance
 
         self.path = []
@@ -23,10 +24,12 @@ class PathExecution:
         self.current_speed = 0
         self.previous_move_time = 0
 
+        self.counter = 0
+
     def update_path(self):
         if not self.path:
             return 
-        if distance(self.path[-1], self.env.checkpoints[self.current_checkpoint_idx]) > 0.03:
+        if distance(self.path[-1], self.env.checkpoints[self.current_checkpoint_idx]) > self.env.robot.distance_allowence:
             self.path = []
             return
         for node in self.path:
@@ -36,9 +39,9 @@ class PathExecution:
                     self.path = []
                     return
 
+
     def update_lookahead_point(self):
         min_dist = float('inf')
-        
         idx = 0
         for i in range(len(self.path)):
             dist = distance(self.path[i], self.env.robot)
@@ -46,27 +49,66 @@ class PathExecution:
                 min_dist = dist
                 idx = i        
                 self.current_lookahead_distance = dist
+
+        if distance(self.env.robot, self.env.checkpoints[self.current_checkpoint_idx]) < min_dist:
+            self.current_lookahead_distance = dist
+            self.lookahead_point = (self.env.checkpoints[self.current_checkpoint_idx].x,
+                                     self.env.checkpoints[self.current_checkpoint_idx].y)
+            return
+
                 
-        
+        # Define coefficients for the cubic interpolation
+        # a = 2 * (self.max_lookahead_distance - self.min_lookahead_distance) / (np.pi / 2)**3
+        a = (self.min_lookahead_distance - self.max_lookahead_distance) / (np.pi / 2) ** (1/3)
+        d = self.max_lookahead_distance
+
         for i in range(idx + 1, len(self.path)):
             dist = distance(self.env.robot, self.path[i])
-            if dist < 0.1:
+            if dist < self.min_lookahead_distance:
                 idx = i
                 self.current_lookahead_distance = dist
                 continue
+            
             dx = self.path[i].x - self.env.robot.x
             dy = self.path[i].y - self.env.robot.y
-        
+            
             angle_difference = np.arctan2(dy, dx) - self.env.robot.a
             angle_difference = np.arctan2(np.sin(angle_difference), np.cos(angle_difference))
-            if dist < self.max_lookahead_distance and abs(angle_difference) < self.env.robot.lookahead_point_distancing:
+            
+            # Calculate the lookahead distance using cubic interpolation
+            probable_lookahead = a * abs(angle_difference)**(1/3) + d
+            
+            if dist < probable_lookahead:
                 idx = i
                 self.current_lookahead_distance = dist
-            
-        # TODO: Add interpolation to get exact lookahead
+        # Set the lookahead point
         self.lookahead_point = (self.path[idx].x, self.path[idx].y)
-        return  self.path[idx].curvature
+    
+        dist = distance(self.env.robot, self.env.checkpoints[self.current_checkpoint_idx])
+        if dist < self.min_lookahead_distance:
+            self.current_lookahead_distance = dist
+            self.lookahead_point = (self.env.checkpoints[self.current_checkpoint_idx].x,
+                                     self.env.checkpoints[self.current_checkpoint_idx].y)
+            return
         
+        dx = self.env.checkpoints[self.current_checkpoint_idx].x - self.env.robot.x
+        dy = self.env.checkpoints[self.current_checkpoint_idx].y - self.env.robot.y
+        
+        angle_difference = np.arctan2(dy, dx) - self.env.robot.a
+        angle_difference = np.arctan2(np.sin(angle_difference), np.cos(angle_difference))
+        
+        # Calculate the lookahead distance using cubic interpolation
+        probable_lookahead = a * abs(angle_difference)**(1/3) + d
+        
+        if dist < probable_lookahead:
+            self.current_lookahead_distance = dist
+            self.lookahead_point = (self.env.checkpoints[self.current_checkpoint_idx].x,
+                                     self.env.checkpoints[self.current_checkpoint_idx].y)
+            return
+
+ 
+
+
     def get_closest_node_curvature(self):
         min_dist = 100
         closest_node = self.path[0]
@@ -94,10 +136,10 @@ class PathExecution:
             angle_difference = self.env.robot.a - self.env.checkpoints[self.current_checkpoint_idx].a
             angle_difference = np.arctan2(np.sin(angle_difference), np.cos(angle_difference))
             if abs(angle_difference) > self.env.robot.angle_allowence:
-                if abs(angle_difference) < np.deg2rad(5):
-                    angular_speed = self.env.robot.minimal_angular_speed
-                else:
-                    angular_speed = self.env.robot.max_angular_speed 
+                # if abs(angle_difference) < np.deg2rad(5):
+                    # angular_speed = self.env.robot.minimal_angular_speed
+                # else:
+                angular_speed = self.env.robot.max_angular_speed 
                 direction = -np.sign(angle_difference)
                 self.get_to_desired_speed(0)
                 return (self.current_speed, direction * angular_speed)
@@ -122,15 +164,13 @@ class PathExecution:
         between_angle = np.arctan2(np.sin(between_angle), np.cos(between_angle))
         direction = -np.sign(between_angle)
         
-
         lookahead_curvature = (2*x) / (self.current_lookahead_distance**2)
-        distance_to_lookahead = np.sqrt(dx**2 + dy**2)
         
         if abs(between_angle) > np.pi / 2:
             linear_speed_scale = 0
         else:
             linear_speed_scale = ((np.pi / 2) - abs(between_angle)) / \
-                (np.pi / 2) * distance_to_lookahead / self.max_lookahead_distance
+                (np.pi / 2) * self.current_lookahead_distance / self.max_lookahead_distance
             
         
         linear_speed = self.env.robot.linear_speed * linear_speed_scale
@@ -149,6 +189,9 @@ class PathExecution:
         return(self.current_speed, angular_speed)
         
     def get_current_move(self):
+        # if self.env.found_finish:
+        #     self.get_to_desired_speed(0)
+        #     return (self.current_speed, 0)
         if not self.path:
             # Still have checkpoints to go through
             if self.current_checkpoint_idx < len(self.env.checkpoints):                
@@ -159,15 +202,19 @@ class PathExecution:
                     self.get_to_desired_speed(0)
                     return (self.current_speed, 0)
             else:
-                if not self.env.found_finish:
+                if not self.env.found_finish and self.counter > 10:
+                    print("Adding a new checkpoint for exploration")
                     exploration_distance = 0.40
                     # self.get_to_desired_speed(0)
                     new_x = self.env.robot.x + np.cos(self.env.robot.a) * exploration_distance
                     new_y = self.env.robot.y + np.sin(self.env.robot.a) * exploration_distance
                     new_a = float(self.env.robot.a)
                     self.env.checkpoints.append(Checkpoint(new_x, new_y, new_a))
+                    self.counter = 0
+                    return (self.current_speed, 0)
                 else:
                     self.get_to_desired_speed(0)
+                    self.counter += 1
                 return (self.current_speed, 0)
         # Here the path is guaranteed to be non-empty
         return self.move_through_path()