9ad7b4ad8716ed01a531982f8e1bc14e9d645852
OlehProof <olegboures@ukr.net>
2024-05-17 19:17:04 +0200

    "Add area checking"

--- Uleh/utils.py
+++ Uleh/utils.py
@@ -30,22 +30,27 @@ def is_within_range(value, range_bounds):
     return range_bounds[0] <= value <= range_bounds[1]
 
 # TODO: rewrite this function
-def is_within_range_distance(aspect_ratio, distance):
+def is_within_range_distance(aspect_ratio, distance, area):
     is_in_range = False
     if (is_within_range(distance, [0.15, 0.5]) and
-        is_within_range(aspect_ratio, [2, 4])):
+        is_within_range(aspect_ratio, [2, 4]) and
+        is_within_range(area, [15000, 70000])):
         is_in_range = True
     elif (is_within_range(distance, [0.3, 1]) and
-        is_within_range(aspect_ratio, [3, 6.2])):
+        is_within_range(aspect_ratio, [3, 6.2]) and
+        is_within_range(area, [4000, 60000])):
         is_in_range = True
     elif (is_within_range(distance, [1, 1.3]) and
-        is_within_range(aspect_ratio, [3.1, 6.8])):
+        is_within_range(aspect_ratio, [3.1, 6.8]) and
+        is_within_range(area, [2000, 10000])):
         is_in_range = True
     elif (is_within_range(distance, [1.3, 2]) and
-    is_within_range(aspect_ratio, [3.3, 7.8])):
+        is_within_range(aspect_ratio, [3.3, 7.8]) and
+        is_within_range(area, [800, 2000])):
         is_in_range = True
     elif (is_within_range(distance, [2, 3]) and
-        is_within_range(aspect_ratio, [3.7, 8])):
+        is_within_range(aspect_ratio, [3.7, 8]) and
+        is_within_range(area, [800, 1700])):
         is_in_range = True
     if is_in_range == False:
         print(f"Rectangle outside boundaries - aspect ratio: {aspect_ratio}, distance: {distance}")
